58202c5ff6d43ac87e012a627b40b720d57bdb69
OH JAE HWAN <ojh102@gmail.com>
2019-08-23 00:30:11 +0900

    navigation fragment ktx 추가, store 보일러플레이트

--- app/src/main/java/com/github/ojh102/timary/ui/write/store/StoreFragment.kt
+++ app/src/main/java/com/github/ojh102/timary/ui/write/store/StoreFragment.kt
@@ -2,6 +2,7 @@ package com.github.ojh102.timary.ui.write.store
 
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.github.ojh102.timary.R
 import com.github.ojh102.timary.base.BaseFragment
 import com.github.ojh102.timary.databinding.FragmentStoreBinding
@@ -10,6 +11,8 @@ internal class StoreFragment : BaseFragment<FragmentStoreBinding>() {
     override val layoutRes = R.layout.fragment_store
 
     private val viewModel by viewModels<StoreViewModel> { viewModelFactory }
+
+    private val args by navArgs<StoreFragmentArgs>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
--- app/src/main/java/com/github/ojh102/timary/ui/write/store/StoreViewModel.kt
+++ app/src/main/java/com/github/ojh102/timary/ui/write/store/StoreViewModel.kt
@@ -1,6 +1,16 @@
 package com.github.ojh102.timary.ui.write.store
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.github.ojh102.timary.base.BaseViewModel
+import com.github.ojh102.timary.ui.legacy.write.store.StoreItem
 import javax.inject.Inject
 
-internal class StoreViewModel @Inject constructor() : BaseViewModel()
+internal class StoreViewModel @Inject constructor() : BaseViewModel() {
+    private val _storeItem = MutableLiveData<StoreItem>()
+    val storeItem: LiveData<StoreItem> = _storeItem
+
+    fun storeCapsule() {
+
+    }
+}
