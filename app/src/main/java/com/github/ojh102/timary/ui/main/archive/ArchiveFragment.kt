57f40560e72898d2870a903830e99553d0609157
OH JAE HWAN <ojh102@gmail.com>
2019-08-25 15:50:00 +0900

    아카이브 완

--- app/src/main/java/com/github/ojh102/timary/ui/main/archive/ArchiveFragment.kt
+++ app/src/main/java/com/github/ojh102/timary/ui/main/archive/ArchiveFragment.kt
@@ -2,15 +2,22 @@ package com.github.ojh102.timary.ui.main.archive
 
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.github.ojh102.timary.EventObserver
 import com.github.ojh102.timary.R
 import com.github.ojh102.timary.base.BaseFragment
 import com.github.ojh102.timary.databinding.FragmentArchiveBinding
-import com.github.ojh102.timary.model.realm.Capsule
+import com.github.ojh102.timary.model.Capsule
 import com.github.ojh102.timary.ui.legacy.main.archieve.ArchiveAdapter
 import javax.inject.Inject
 
 internal class ArchiveFragment : BaseFragment<FragmentArchiveBinding>() {
     override val layoutRes = R.layout.fragment_archive
+
+    override val navController: NavController
+        get() = activity!!.findNavController(R.id.nav_host_fragment)
 
     private val viewModel by viewModels<ArchiveViewModel> { viewModelFactory }
 
@@ -23,6 +30,10 @@ internal class ArchiveFragment : BaseFragment<FragmentArchiveBinding>() {
         binding.viewModel = viewModel
 
         initRecyclerView()
+
+        viewModel.navigateToRead.observe(this, EventObserver { navController.navigate(it) })
+
+        viewModel.loadArchiveCapsules()
     }
 
     private fun initRecyclerView() {
@@ -32,9 +43,11 @@ internal class ArchiveFragment : BaseFragment<FragmentArchiveBinding>() {
         }
 
         archiveAdapter.setCallbacks(object : ArchiveAdapter.Callbacks {
-            override fun onClickArchiveCapsule(capsule: com.github.ojh102.timary.model.Capsule) {
-
+            override fun onClickArchiveCapsule(capsule: Capsule) {
+                viewModel.onClickArchiveCapsule(capsule)
             }
         })
+
+        viewModel.capsules.observe(this) { archiveAdapter.submitList(it) }
     }
 }
--- app/src/main/java/com/github/ojh102/timary/ui/main/archive/ArchiveViewModel.kt
+++ app/src/main/java/com/github/ojh102/timary/ui/main/archive/ArchiveViewModel.kt
@@ -1,9 +1,60 @@
 package com.github.ojh102.timary.ui.main.archive
 
+import android.content.Context
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.TextAppearanceSpan
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavDirections
+import com.github.ojh102.timary.Event
+import com.github.ojh102.timary.R
 import com.github.ojh102.timary.base.BaseViewModel
+import com.github.ojh102.timary.model.Capsule
+import com.github.ojh102.timary.repository.LocalRepository
+import com.github.ojh102.timary.ui.main.MainFragmentDirections
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 import javax.inject.Inject
 
-internal class ArchiveViewModel @Inject constructor() : BaseViewModel() {
-    val headerText = MutableLiveData<CharSequence>()
+internal class ArchiveViewModel @Inject constructor(
+    private val context: Context,
+    private val localRepository: LocalRepository
+) : BaseViewModel() {
+    private val _headerText = MutableLiveData<CharSequence>()
+    val headerText: LiveData<CharSequence> = _headerText
+
+    private val _capsules = MutableLiveData<List<Capsule>>()
+    val capsules: LiveData<List<Capsule>> = _capsules
+
+    private val _navigateToRead = MutableLiveData<Event<NavDirections>>()
+    val navigateToRead: LiveData<Event<NavDirections>> = _navigateToRead
+
+    fun loadArchiveCapsules() {
+        viewModelScope.launch(Dispatchers.IO) {
+            localRepository.getCapsules()
+                .collect {
+                    launch(Dispatchers.Main) {
+                        _headerText.value = SpannableString(context.getString(R.string.format_archive_header, it.size))
+                            .apply {
+                                setSpan(
+                                    TextAppearanceSpan(context, R.style.B16Grape),
+                                    0,
+                                    it.size.toString().length,
+                                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                                )
+                            }
+
+                        _capsules.value = it
+                    }
+                }
+        }
+    }
+
+    fun onClickArchiveCapsule(capsule: Capsule) {
+        _navigateToRead.value = Event(MainFragmentDirections.actionMainFragmentToReadFragment())
+    }
 }
